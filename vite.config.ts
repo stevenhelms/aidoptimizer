e6c3213ac98e46fbd83f349ebed67c24389fb951
Steve Helms <steve@helms.place>
2024-12-11 21:50:00 -0600

    Switching to Vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: '/',
+        plugins: [react()],
+        define: {
+          'process.env.YOUR_STRING_VARIABLE': JSON.stringify(env.YOUR_STRING_VARIABLE),
+          'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
+          // If you want to exposes all env variables, which is not recommended
+          // 'process.env': env
+        },
+    };
+});
