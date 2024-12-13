24114daa8d1a49f7bd0efc29a4ade26b962ae349
Steve Helms <steve@helms.place>
2024-12-13 10:56:21 -0600

    removing registerUser import

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUser, userLogin } from "./authActions";
+import { userLogin } from "./authActions";
 
 // initialize userToken from local storage
 const userToken = localStorage.getItem("userToken")
